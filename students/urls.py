d5ac912f702979a6c72d14a13ccaa9c764cadf0a
Charles MacDonald-Smith <ch@rlie.xyz>
2022-12-08 01:31:37 +0000

    Add new student

--- students/urls.py
+++ students/urls.py
@@ -5,4 +5,5 @@ from students import views
 app_name = 'students'
 urlpatterns = [
     path('', views.StudentListView.as_view(), name='list'),
+    path('new/', views.StudentCreateView.as_view(), name='create'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import login_required, user_passes_test
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 from auth.roles import is_lecturer
+from students.forms import StudentForm
 
 
 @method_decorator(login_required, name='dispatch')
@@ -13,3 +15,19 @@ class StudentListView(ListView):
     template_name = 'students/student_list.html'
     context_object_name = 'students'
     ordering = ['last_name']
+
+
+@method_decorator(login_required, name='dispatch')
+@method_decorator(user_passes_test(is_lecturer), name='dispatch')
+class StudentCreateView(CreateView):
+    model = User
+    template_name = 'students/student_create.html'
+    form_class = StudentForm
+    success_url = reverse_lazy('students:list')
+
+    def form_valid(self, form):
+        students = Group.objects.get(name='student')
+        form.instance.save()
+        form.instance.groups.add(students)
+        return super(StudentCreateView, self).form_valid(form)
+
