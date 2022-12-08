2d90ff9390e78809dc166ca3936979af9e0b9f1f
Charles MacDonald-Smith <ch@rlie.xyz>
2022-12-08 10:19:47 +0000

    Add dummy user in setup

--- auth/management/commands/creategroups.py
+++ auth/management/commands/creategroups.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import Group
 from django.core.management import BaseCommand
+from django.contrib.auth.models import User
 
 
 class Command(BaseCommand):
@@ -10,3 +11,10 @@ class Command(BaseCommand):
         for group in groups:
             Group.objects.get_or_create(name=group)
             print(f"Created group {group}")
+
+        # if alice does not exist
+        if not User.objects.filter(username='alice').exists():
+            User.objects.create_user(username='alice', password='alice2022')
+            user = User.objects.get(username='alice')
+            group = Group.objects.get(name='student')
+            user.groups.add(group)
